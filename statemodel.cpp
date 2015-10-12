7ebd45285add3f81d01f8083a8c39cef60b3fd76
Deepanker Mishra <deepanker27mishra@gmail.com>
2015-10-12 22:38:18 +0530

    state model

---
+++ statemodel.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+#include<map>
+
+using namespace std ;
+
+class State{
+	vector<int>dealerCards;
+	vector<int>playerCards;
+	bool isSlit;
+	bool nextAction;
+};
+
+
