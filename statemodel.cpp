f70f2f665b1fe54150b19bc53577a56b7b41d78f
Deepanker Mishra <deepanker27mishra@gmail.com>
2015-10-12 22:49:56 +0530

    1
    update1

--- statemodel.cpp
+++ statemodel.cpp
@@ -5,10 +5,29 @@
 using namespace std ;
 
 class State{
+	bool canSlit;
+	bool final;
+	int value ;
 	vector<int>dealerCards;
 	vector<int>playerCards;
-	bool isSlit;
-	bool nextAction;
 };
 
+//state change by actions
+void S(State s){ //stand
+	return ;
+}
+void H(State s){ //hit
+	return ;
+}
 
+void D(State s){ //double
+ 	return ;
+}
+
+void P(state s){ //sPlit
+	return ;
+}
+
+int main(){
+	return 0 ;
+}
